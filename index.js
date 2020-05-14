a6d38ca67765e4dde7f59bad2fb17aaf216bc223
Adriaan Pelzer <adriaan@wewillraakyou.com>
2020-05-14 11:40:20 +0100

    check local fs

--- index.js
+++ index.js
@@ -1,7 +1,44 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const H = require('highland');
+const aws = require('aws-sdk');
+const fs = require('fs');
 
-console.log(process.env);
+const cfn = H.streamifyAll(new aws.CloudFormation());
+
+return H.wrapCallback(fs.readdir)('./')
+  .doto(() => core.setOutput('time', new Date().toTimeString()))
+  .errors(error => core.setFailed(error.message))
+  .each(console.log);
+/*
+      - name: cloudformation deploy
+        env:
+          TEMPLATE: cfn-template.yml
+          AWS_STACK_NAME: ${{ needs.setup.outputs.repository-name }}
+          CAPABILITIES: CAPABILITY_NAMED_IAM
+          PARAMETER_OVERRIDES: ContainerImageUrl=${{ steps.build-image.outputs.image-url }} IntegrationTestLambdaS3Key=${{ steps.build-tests.outputs.s3-key }} Environment=Dev
+        run: |
+          AWS_CFN_CMD="aws cloudformation"
+          STACK_ARG="--stack-name ${AWS_STACK_NAME}"
+          if ${AWS_CFN_CMD} describe-stacks ${STACK_ARG} > /dev/null 2>&1; then
+            CFN_STATE=$(${AWS_CFN_CMD} describe-stacks ${STACK_ARG} --query 'Stacks[0].StackStatus' --output text)
+            if [[ "${CFN_STATE}" == "ROLLBACK_COMPLETE" ]]; then
+              echo "cfn state is ${CFN_STATE}: deleting ..."
+              ${AWS_CFN_CMD} delete-stack ${STACK_ARG}
+              ${AWS_CFN_CMD} wait stack-delete-complete ${STACK_ARG}
+            else
+              while [[ "${CFN_STATE}" != *"_COMPLETE" ]]; do
+                echo "cfn state is ${CFN_STATE}: waiting ..."
+                sleep 10
+                CFN_STATE=$(${AWS_CFN_CMD} describe-stacks ${STACK_ARG} --query 'Stacks[0].StackStatus' --output text)
+                if [[ "${CFN_STATE}" == *"_FAILED" ]]; then
+                  echo "cfn state is ${CFN_STATE}: first fix this and deploy again"
+                  exit 1
+                fi
+              done
+            fi
+          fi
+          ${AWS_CFN_CMD} deploy --template-file ${TEMPLATE} ${STACK_ARG} --parameter-overrides ${PARAMETER_OVERRIDES} --capabilities ${CAPABILITIES} --no-fail-on-empty-changeset
 
 try {
   // `who-to-greet` input defined in action metadata file
@@ -14,4 +51,4 @@ try {
   console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
-}
+}*/
